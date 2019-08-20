e67eeb1d1ec52bb46731f83810723c2ebb02547a
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-20 08:57:43 -0400

    Add router to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
+import Home from './Home/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pong/">pong</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/Home/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = (prop) => {
+    return (<div></div>)
+}
+
+export default Home;
