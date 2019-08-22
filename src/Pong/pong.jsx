d5532da9ea73bb8eca32464ad92cebb982bd900d
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-21 21:28:52 -0400

    Add material-ui to project

--- src/Pong/pong.jsx
+++ src/Pong/pong.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Pong = (prop) => {
-    return (<div>Pong</div>)
+    return (
+        <div>
+            <h1>Pong</h1>
+            <canvas ></canvas>
+        </div>
+    );
 }
 
 export default Pong;
