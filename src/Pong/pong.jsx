134184cf4a51be0de73052ba21da72f9c26aa6b4
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-26 17:08:03 -0400

    Add game container

--- src/Pong/pong.jsx
+++ src/Pong/pong.jsx
@@ -2,13 +2,19 @@ import React, { Fragment } from 'react';
 import { Typography } from '@material-ui/core';
 
 const Pong = (prop) => {
-    
+
 
     return (
         <Fragment>
             <Typography variant="h2">
                 Pong
             </Typography>
+            <div style={{ textAlign: 'center', width:'100%',border: '1px solid red', marginTop:'30px'}}>
+                <div style={{ width: 800, height: 600, border: '1px solid black', textAlign: 'center', display: 'inline-block'}}>
+
+                </div>
+
+            </div>
         </Fragment>
     );
 }
